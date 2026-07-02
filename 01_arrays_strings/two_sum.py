c05b2e3907df23eb156ad6d769cf02d2772074d2
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-07-02 17:36:18 +0530

    Update two_sum.py

--- 01_arrays_strings/two_sum.py
+++ 01_arrays_strings/two_sum.py
@@ -1,3 +1,5 @@
+# HashTable Approach 1
+
 class Solution:
     def twoSum(self, nums, target):
         seen = {}
@@ -7,3 +9,35 @@ class Solution:
                 return [seen[diff], i]
             seen[n] = i
         return []
+
+# HashTable Approach 2
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            d[nums[i]] = i
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if (need in d.keys and d[need]!=i):
+                return [i,d[need]]
+
+# Brute Force Approach 1
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[i]+nums[j]==target):
+                    return [i,j]
+
+# Brute Force Approach 2
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if (need in nums and nums.index(need)!=i):
+                return [i,nums.index(need)]
+
+# EOF
